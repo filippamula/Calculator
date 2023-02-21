f5f252ac67a2a1e3eb3e0cc3292485ed61b05b61
filip <filippamula@onet.pl>
2023-02-21 20:29:45 +0100

    added exception catching on value input

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,11 +5,26 @@ public class Calculator {
     public void run(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Podaj pierwsza liczbę");
-        float a = sc.nextFloat();
-        System.out.println("Podaj drugą liczbe");
-        float b = sc.nextFloat();
-        System.out.println("Podaj operację");
+        float a ;
+        try{
+            a = sc.nextFloat();
+        }
+        catch (Exception e){
+            System.out.println("Błąd - podano złą wartość");
+            return;
+        }
 
+        System.out.println("Podaj drugą liczbe");
+        float b;
+        try{
+            b = sc.nextFloat();
+        }
+        catch (Exception e){
+            System.out.println("Błąd - podano złą wartość");
+            return;
+        }
+
+        System.out.println("Podaj operację");
         try {
             System.out.println(String.join(", ", Arrays.stream(Operation.values()).map(Operation::toString).toList()));
             Operation o =  Operation.of(sc.next());

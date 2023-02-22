7cd85ba4e29a84b53d71c9540a3af2f3b24678c2
filip <filippamula@onet.pl>
2023-02-22 19:25:43 +0100

    initial

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,22 +1,45 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Calculator {
     public void run(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj pierwsza liczbę");
-        float a = sc.nextFloat();
-        System.out.println("Podaj drugą liczbe");
-        float b = sc.nextFloat();
-        System.out.println("Podaj operację");
+        System.out.println("Podaj wyrażenie do obliczenia w kolejności wykonywania działań");
+        String input = sc.next();
 
-        try {
-            System.out.println(String.join(", ", Arrays.stream(Operation.values()).map(Operation::toString).toList()));
-            Operation o =  Operation.of(sc.next());
+        StringBuilder number = new StringBuilder();
+        float result = 0;
+        Operation o = Operation.ADDITION;
 
-            System.out.println(new Equation(a, b).calculate(o));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        for (int i = 0; i < input.length(); i++){
+            if(isNumeric(input.charAt(i))){
+                number.append(input.charAt(i));
+            }
+            if (isOperator(input.charAt(i))){
+                result = new Equation(result, Float.parseFloat(number.toString())).calculate(o);
+                number.setLength(0);
+                o = Operation.of(String.valueOf(input.charAt(i)));
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static boolean isNumeric(char value) {
+        try{
+            Integer.parseInt(String.valueOf(value));
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public static boolean isOperator(char value){
+        try{
+            Operation.of(String.valueOf(value));
+            return true;
+        }catch(Exception e){
+            return false;
         }
     }
 }
+

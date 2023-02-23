058e6fc706a46fd4502cf4ff795300c69c3ba246
filip <filippamula@onet.pl>
2023-02-23 21:05:10 +0100

    initial

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,29 +1,93 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Calculator {
     public void run(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj wyrażenie do obliczenia w kolejności wykonywania działań");
+        System.out.println("Podaj wyrażenie do obliczenia");
         String input = sc.next();
+        Queue<String> onp = toOnp(input);
 
-        StringBuilder number = new StringBuilder();
-        float result = 0;
-        Operation o = Operation.ADDITION;
-
-        for (int i = 0; i < input.length(); i++){
-            if(isNumeric(input.charAt(i))){
-                number.append(input.charAt(i));
+        Stack<Float> stack = new Stack<>();
+        for(var x : onp){
+            if(isNumeric(x)){
+              stack.push(Float.parseFloat(x));
             }
-            if (isOperator(input.charAt(i))){
-                result = new Equation(result, Float.parseFloat(number.toString())).calculate(o);
-                number.setLength(0);
-                o = Operation.of(String.valueOf(input.charAt(i)));
+            if(isOperator(x)){
+                float a = stack.pop();
+                float b = stack.pop();
+
+                stack.push(new Equation(a,b).calculate(Operation.of(x)));
             }
         }
-
-        System.out.println(result);
+        System.out.println(stack.pop());
     }
 
+    public static Queue<String> toOnp(String equation){
+        StringBuilder number = new StringBuilder();
+        Queue<String> exit = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < equation.length(); i++){
+            if(isNumeric(equation.charAt(i)) || equation.charAt(i) == '.'){
+                number.append(equation.charAt(i));
+            }
+            if(isOperator(equation.charAt(i))){
+                if(number.length() > 0){
+                    exit.add(number.toString());
+                    number.setLength(0);
+                }
+
+                if(!stack.empty()){
+                    while (getPriority(stack.peek()) >= getPriority(equation.charAt(i))) {
+                        exit.add(stack.pop().toString());
+                        if(stack.empty()){
+                            break;
+                        }
+                    }
+                }
+                stack.push(equation.charAt(i));
+            }
+            if(equation.charAt(i) == '('){
+                stack.push(equation.charAt(i));
+            }
+            if(equation.charAt(i) == ')'){
+                exit.add(number.toString());
+                number.setLength(0);
+                if(!stack.empty()){
+                    while (stack.peek() != '('){
+                        exit.add(stack.pop().toString());
+                        if(stack.empty()){
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        exit.add(number.toString());
+
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            if (stack.peek() == '(' || stack.peek() == ')') {
+                stack.pop();
+                continue;
+            }
+            exit.add(String.valueOf(stack.pop()));
+        }
+
+        return exit;
+    }
+
+    public static int getPriority(char operator){
+        return switch (operator) {
+            case 'r', '(' -> 0;
+            case '+', '-', ')' -> 1;
+            case '*', '/' -> 2;
+            default -> 100;
+        };
+    }
     public static boolean isNumeric(char value) {
         try{
             Integer.parseInt(String.valueOf(value));
@@ -33,9 +97,27 @@ public class Calculator {
         }
     }
 
+    public static boolean isNumeric(String value) {
+        try{
+            Integer.parseInt(value);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
     public static boolean isOperator(char value){
         try{
             Operation.of(String.valueOf(value));
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public static boolean isOperator(String value){
+        try{
+            Operation.of(value);
             return true;
         }catch(Exception e){
             return false;

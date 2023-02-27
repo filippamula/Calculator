3dd624116746eb330ab3e3c0baa9b3ef33d0fc43
filip <filippamula@onet.pl>
2023-02-27 20:13:13 +0100

    added support for complex equations

--- src/Calculator.java
+++ src/Calculator.java
@@ -22,40 +22,118 @@ public class Calculator {
 
         while (file.hasNext()){
             String equation = file.next();
-            float a = 0,b;
-            Operation o = Operation.ADDITION;
-            StringBuilder number = new StringBuilder();
+            Queue<String> equationOnp = toOnp(equation);
 
-            for (var item : equation.toCharArray()) {
-                if (isOperator(item)){
-                    o = Operation.of(String.valueOf(item));
-                    try{
-                        a = Float.parseFloat(number.toString());
-                    }catch (NumberFormatException e){
-                        System.out.println("Błąd - żle podana liczba");
-                        return;
-                    }
+            pw.println(CalculateOnp(equationOnp));
+        }
+        pw.close();
+    }
+    public static float CalculateOnp(Queue<String> equation){
+
+        Stack<Float> stack = new Stack<>();
+        for(var x : equation){
+            if(isNumeric(x)){
+                stack.push(Float.parseFloat(x));
+            }
+            if(isOperator(x)){
+                float b = stack.pop();
+                float a = stack.pop();
+
+                stack.push(new Equation(a,b).calculate(Operation.of(x)));
+            }
+        }
+
+        return stack.pop();
+    }
+    public static Queue<String> toOnp(String equation){
+        StringBuilder number = new StringBuilder();
+        Queue<String> exit = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < equation.length(); i++){
+            if(isNumeric(equation.charAt(i)) || equation.charAt(i) == '.'){
+                number.append(equation.charAt(i));
+            }
+            if(isOperator(equation.charAt(i))){
+                if(number.length() > 0){
+                    exit.add(number.toString());
                     number.setLength(0);
                 }
 
-                number.append(item);
+                if(!stack.empty()){
+                    while (getPriority(stack.peek()) >= getPriority(equation.charAt(i))) {
+                        exit.add(stack.pop().toString());
+                        if(stack.empty()){
+                            break;
+                        }
+                    }
+                }
+                stack.push(equation.charAt(i));
             }
-
-            try {
-                b = Float.parseFloat(number.toString());
-            }catch (NumberFormatException e){
-                System.out.println("Błąd - źle podana liczba");
-                return;
+            if(equation.charAt(i) == '('){
+                stack.push(equation.charAt(i));
             }
-
-
-            pw.println(equation + "=" + new Equation(a,b).calculate(o));
+            if(equation.charAt(i) == ')'){
+                exit.add(number.toString());
+                number.setLength(0);
+                if(!stack.empty()){
+                    while (stack.peek() != '('){
+                        exit.add(stack.pop().toString());
+                        if(stack.empty()){
+                            break;
+                        }
+                    }
+                }
+            }
         }
-        pw.close();
+        exit.add(number.toString());
+
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            if (stack.peek() == '(' || stack.peek() == ')') {
+                stack.pop();
+                continue;
+            }
+            exit.add(String.valueOf(stack.pop()));
+        }
+
+        return exit;
+    }
+    public static int getPriority(char operator){
+        return switch (operator) {
+            case 'r', '(' -> 0;
+            case '+', '-', ')' -> 1;
+            case '*', '/' -> 2;
+            default -> 100;
+        };
+    }
+    public static boolean isNumeric(char value) {
+        try{
+            Integer.parseInt(String.valueOf(value));
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+    public static boolean isNumeric(String value) {
+        try{
+            Integer.parseInt(value);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
     }
     public static boolean isOperator(char value){
         try{
             Operation.of(String.valueOf(value));
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+    public static boolean isOperator(String value){
+        try{
+            Operation.of(value);
             return true;
         }catch(Exception e){
             return false;
